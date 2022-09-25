1e564f27cefc2ae3ef4db56c42889256100f20bd
Magoved <python19871987@mail.ru>
2022-09-25 20:28:25 +0300

    Homework2

---
+++ seminar_1_4/Program.cs
@@ -0,0 +1,28 @@
+﻿void Zadanie1(int number)
+{
+    System.Console.WriteLine(number/10%10);
+}
+Zadanie1(int.Parse(Console.ReadLine()));
+
+
+void Zadanie2(string number)
+{
+    {
+    if (number.Length >= 3)
+        System.Console.WriteLine(number[2]);
+    else
+        System.Console.WriteLine("Третьей цифры нет");
+    }
+}
+Zadanie2(Console.ReadLine());
+
+void Zadanie3(int number)
+{
+    {
+    if (number >= 6)
+        System.Console.WriteLine("Да");
+    else
+        System.Console.WriteLine("Нет");
+    }
+}
+Zadanie3(int.Parse(Console.ReadLine()));
